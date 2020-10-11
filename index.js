4d3e3bf38630d887ca48fd52d3ee1fc8d15f8b6d
Avaer Kazmer <a@modules.io>
2020-10-11 10:52:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-ws = new WebSocket('wss://https-mystifying--artificer-reticulum-io.proxy.exokit.org/socket/websocket?vsn=2.0.0');
-ws.onopen = () => {
-  console.log('open');
-  ws.send(JSON.stringify(["1","1","ret","phx_join",{"hub_id":"UZ7NA7m"}]));
-};
-ws.onerror = err => {
-  console.warn('error', err);
-};
-ws.addEventListener('message', e => {
-  const [a,b,c,d,{response}] = JSON.parse(e.data);
-  const {session_id,vapid_public_key} = response;
-  console.log({session_id, vapid_public_key});
-  ws.send(JSON.stringify(["2","2","hub:UZ7NA7m","phx_join",{"profile":{"avatarId":"t3fZiJ5","displayName":"Long-Tailed-14127"},"push_subscription_endpoint":null,"auth_token":null,"perms_token":null,"context":{"mobile":false,"embed":false},"hub_invite_id":null}]));
-  ws.addEventListener('message', e => {
-    const [a,b,c,d,{response:{hubs:[hub]}}] = JSON.parse(e.data);
-    const {host, port, scene: {model_url}, slug} = hub;
-    console.log('got model url', model_url);
-  }, {once: true});
-}, {once: true});
-1
+const fs = require('fs');
+const http = require('http');
+// const https = require('https');
+const express = require('express');
+
+// const CERT = fs.readFileSync('./certs/cert.pem');
+// const PRIVKEY = fs.readFileSync('./certs/key.pem');
+
+const app = express();
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use(express.static(__dirname));
+
+http.createServer(app)
+  .listen(3000);
+/* https.createServer({
+  cert: CERT,
+  key: PRIVKEY,
+}, app)
+  .listen(3001); */
+
+console.log('http://localhost:3000');
+// console.log('https://localhost:3001');
